0b423847343fb1665e8d64e2bc44fd1e243c913e
AndroidNaveen <naveenandroid6557@gmail.com>
2020-09-21 10:30:35 +0530

    Customer
    Customer

--- OnlineFood/customer/urls.py
+++ OnlineFood/customer/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('customer_register/',views.customer_register,name='customer_register'),
     path('customer_save/',views.customer_save,name='customer_save'),
     path('customer_check_otp/',views.customer_check_otp,name='customer_check_otp'),
+    path('customer_login/',views.customer_login,name='customer_login'),
+    path('customer_login_check/',views.customer_login_check,name='customer_login_check'),
+    path('customer_welcome/<int:pk>/',views.customer_welcome,name='customer_welcome'),
+    path('customer_logout/',views.customer_login_check,name='customer_logout'),
+    path('customer_menu/',views.customer_menu,name='customer_menu'),
     path('add_to_cart/',views.add_to_cart,name='add_to_cart'),
 
 ]
--- OnlineFood/customer/views.py
+++ OnlineFood/customer/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from customer.middleware import returnAddress
 from vendor.models import FoodItemsModel
 from pwn.models import CityModel
@@ -21,9 +21,10 @@ def showIndex(request):
 
 def add_to_cart(request):
     if request.session['customer_status']:
-        pass
+        i_id = request.POST.get("item")
+
     else:
-        pass
+        return redirect('customer_login')
 
 
 def customer_register(request):
@@ -53,6 +54,49 @@ def customer_save(request):
 def customer_check_otp(request):
     try:
         CustomerRegistrationModel.objects.get(contact=request.POST.get("cno"),OTP=request.POST.get("otp"))
-        return render(request,"customer/login.html")
+        return render(request,"customer/welcome.html")
     except CustomerRegistrationModel.DoesNotExist:
         return render(request,"customer/otp.html",{"contactNo":request.POST.get("cno"),"message":"Invalid OTP"})
+
+
+def customer_login(request):
+    return render(request,"customer/login.html")
+
+
+def customer_login_check(request):
+    if request.method == "POST":
+        cno = request.POST.get("c1")
+        pa = request.POST.get("c2")
+        try:
+            res = CustomerRegistrationModel.objects.get(contact=cno, password=pa)
+            request.session["customer_status"] = True
+            request.session["customer_id"] = res.id
+            return redirect('customer_welcome', pk=res.id)
+        except CustomerRegistrationModel.DoesNotExist:
+            return render(request, 'customer/login.html', {"error": "Invalid User"})
+
+    else:
+        request.session["customer_status"] = False
+        return render(request, "customer/login.html", {"error": "Customer Logout Success"})
+
+
+def customer_welcome(request,pk):
+    request.session["vendor_id"] = pk
+    customer_details = CustomerRegistrationModel.objects.get(id=pk)
+    return render(request, "customer/home.html", {"customer_details": customer_details})
+
+
+def customer_menu(request):
+
+    customer_details = CustomerRegistrationModel.objects.get(id=request.session["customer_id"])
+
+    res = returnAddress()
+    page_no = request.GET.get("pageno", 1)
+    data = FoodItemsModel.objects.all()
+    p = Paginator(data, 3)
+    if page_no:
+        page = p.page(page_no)
+    else:
+        page = p.page(1)
+
+    return render(request,"customer/menu.html",{"customer_details": customer_details,"address":res[0], "city":res[1], "food":page})
